93efbc034896f8605cfc32d8564313fc9f5f4835
Fox <51061164+TristanPFox@users.noreply.github.com>
2022-03-06 14:51:17 -0500

    Update gitclone.bat

--- gitclone.bat
+++ gitclone.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
-CMD /k "git clone https://github.com/TristanPFox/DCSWaypoint.git"
+git clone https://github.com/TristanPFox/DCSWaypoint.git
 ECHO =====================
 ECHO Finished Cloning files from Github!
 ECHO Starting install... Press any key to continue immedietly.
 TIMEOUT /t 20
-CMD /k "DCSWaypoint/setup.bat"
+CMD /k "DCSWaypoint/setup.bat
 PAUSE
